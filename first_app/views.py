840393670efcd2243978c98c4bee439416dedebd
suhy <hisuhy065@gmail.com>
2024-07-26 14:45:42 +0900

    기능 추가

--- first_app/views.py
+++ first_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 import random
+from faker import Faker
 
 # Create your views here. 기능 동작
 
@@ -38,3 +39,32 @@ def lotto(request):
     }
 
     return render(request, 'lotto.html', context)
+
+def username(request, name):
+    context = {
+        'name': name,
+    }
+
+    return render(request, 'username.html', context)
+
+def cube(request, number):
+    result = number ** 3
+    
+    context = {
+        'result': result,
+    }
+
+    return render(request, 'cube.html', context)
+
+def posts(request):
+    fake = Faker()
+    fake_posts = []
+
+    for i in range(100):
+        fake_posts.append(fake.text())
+
+    context = {
+        'fake_posts': fake_posts,
+    }
+
+    return render(request, 'posts.html', context)
--- first_pjt/urls.py
+++ first_pjt/urls.py
@@ -26,5 +26,7 @@ urlpatterns = [
     path('hello/', views.hello),
     path('lunch/', views.lunch),
     path('lotto/', views.lotto),
+    path('username/<name>/', views.username),
+    path('cube/<int:number>/', views.cube),
+    path('posts/', views.posts),
 ]
-
